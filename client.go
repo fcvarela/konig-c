ac47a6a1c58e76eab22caef85d15e686f31592f5
Filipe Varela <fcvarela@gmail.com>
2014-06-27 14:30:23 +0100

    Added test client

---
+++ client.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"./graphproto.pb"
+	"code.google.com/p/goprotobuf/proto"
+	"fmt"
+	"log"
+)
+
+func main() {
+	fmt.Println("Starting")
+	stub, client, err := graphproto.DialGraphService("tcp", "127.0.0.1:1984")
+	if err != nil {
+		log.Fatal(`arith.DialGraphService("tcp", "127.0.0.1:1984"):`, err)
+	}
+	defer client.Close()
+
+	var args graphproto.NewGraphRequest
+	var reply graphproto.NewGraphResponse
+
+	for i := 0; i < 1000; i++ {
+		args.Name = proto.String("hello, world")
+		if err = stub.Newgraph(&args, &reply); err != nil {
+			log.Fatal("graphproto error:", err)
+		}
+		fmt.Printf("Result: (%s)=%d\n", args.GetName(), reply.GetRes())
+	}
+}
