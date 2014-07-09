10e8c5b2320365abc568469306dcb063e46b42e2
Filipe Varela <filipe.varela@thomsonreuters.com>
2014-07-09 14:43:57 +0100

    Initial Commit

---
+++ osx-app/Konig/Konig/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Konig
+//
+//  Created by Filipe Varela on 09/07/2014.
+//  Copyright (c) 2014 Filipe Varela. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
