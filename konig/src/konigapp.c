1badcea948b10b8452fe910ec9f432c68b2b5d58
Filipe Varela <filipe.varela@thomsonreuters.com>
2014-07-17 10:57:01 +0100

    Separated app and library

--- konig/src/konigapp.c
+++ konig/src/konigapp.c
@@ -1,17 +1,17 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <konig.h>
+#include <libkonig/konig.h>
 
 uint32_t graph_id = 0;
 
 uint32_t tree(uint32_t parent, uint32_t n) {
     uint32_t vid = konig_graph_add_vertex(graph_id);
-    uint32_t eid = konig_graph_add_edge(graph_id, parent, vid);
+    konig_graph_add_edge(graph_id, parent, vid);
 
     if (n > 0) {
         for (int j=0; j<2; j++) {
-            uint32_t v2 = tree(vid, n-1);
+            tree(vid, n-1);
         }
     }
 
@@ -25,7 +25,7 @@ int main(int argc, char **argv) {
     // binary tree
     graph_id = konig_add_graph();
     uint32_t root_vid = konig_graph_add_vertex(graph_id);
-    uint32_t last_vid = tree(root_vid, 10);
+    tree(root_vid, 10);
 
     // circle
     while (!konig_done()) {
